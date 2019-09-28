0e149c85bbefc1dd7cfca7962636d0524ebe0a4a
Lori Humphries <lhemailme@aim.com>
2019-09-28 11:16:24 -0400

    moving our requests to a service

---
+++ src/app/posts.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+export class PostsService{
+
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content}
+        this.http
+        .post<{ name: string }>(
+          'https://ng-complete-guide-c111d.firebaseio.com/posts.json',
+          postData
+        )
+        .subscribe(responseData => {
+          console.log(responseData);
+        });
+
+    }
+
+    fetchPosts() {
+     return this.http
+    .get<{ [key: string]: Post }>('https://ng-complete-guide-c111d.firebaseio.com/posts.json')
+    .pipe(
+      map(responseData => {
+      const postsArray: Post[] = [];
+      for(const key in responseData) {
+        if(responseData.hasOwnProperty(key)) {
+          postsArray.push({...responseData[key], id: key});
+        }
+       }
+       return postsArray;
+    })
+    );
+   
+    }
+}
